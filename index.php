cd1ce3dbbe4496d4dec38908fd1464f04eb57bce
P0lle <Malthe@Grundtvigsvej.dk>
2017-11-17 21:12:32 +0100

    Setup

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="utf-8">
+    <title>Live</title>
+    <script type="text/javascript" src="js/script.js"></script>
+  </head>
+
+  <body>
+    <h1>Liveresultater</h1>
+  </body>
+</html>
