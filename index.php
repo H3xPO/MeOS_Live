aabc68ac08f58bb1a7f4b3b6e002b109989387ac
P0lle <Malthe@Grundtvigsvej.dk>
2017-11-17 22:54:26 +0100

    Udregning af tid i tidsformat udfra selvdefinerede variabler

--- index.php
+++ index.php
@@ -7,7 +7,18 @@
     <script type="text/javascript" src="js/script.js"></script>
   </head>
 
+  <?php
+    //Udregning af måltid (Oscar B.)
+    $zero = 54000;
+    $start = 67500-$zero;
+    $finish = 16164;
+
+    $timeint = ($finish - $start);
+    $timereal = gmdate("H:i:s", $timeint);
+   ?>
+
   <body>
     <h1>Liveresultater</h1>
+    <?php echo "Din tid er $timereal"; ?>
   </body>
 </html>
