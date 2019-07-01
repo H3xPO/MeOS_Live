ab509d4b89cd8889883000abf0e7a1eb7594d9ff
H3xPO <33764125+H3xPO@users.noreply.github.com>
2019-07-01 22:20:13 +0200

    Opdateret med IISExpress til afvikling

---
+++ udvikling/start.bat
@@ -0,0 +1,6 @@
+set webFolder=..\udvikling\version_200
+call set webFolder=%%webFolder:IISExpress=Web%%
+cd ..\IISExpress
+start iisexpress.exe /path:"%webFolder%" /port:8181
+timeout 1
+start http://localhost:8181/Default.aspx
