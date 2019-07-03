bd6d3c42bd3e4ec253d30389a5dbdc1eab377bd2
H3xPO <33764125+H3xPO@users.noreply.github.com>
2019-07-03 22:58:37 +0200

    Update openfirewall.bat

--- openfirewall.bat
+++ openfirewall.bat
@@ -2,8 +2,8 @@
 ECHO:
 setlocal
 set port=8181
-netsh http add urlacl url=http://*:8181/ user=everyone >nul
-netsh http add urlacl url=http://*:8181/ user=alle >nul
+netsh http add urlacl url=http://*:%port%/ user=everyone >nul
+netsh http add urlacl url=http://*:%port%/ user=alle >nul
 ECHO Din firewall tillader nu trafik gennem port %port%
 endlocal
 pause >nul
