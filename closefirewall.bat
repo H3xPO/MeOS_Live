9fa5542eaff6e6bfd5031ba712289f05c5c53105
H3xPO <33764125+H3xPO@users.noreply.github.com>
2019-08-03 19:10:57 +0200

    Clean up in files

--- closefirewall.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-ECHO:
-setlocal
-set port=8181
-netsh http delete urlacl url=http://*:%port%/ >nul
-ECHO Din firewall blokerer igen port %port%
-endlocal
-pause >nul
--- openfirewall.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-ECHO:
-setlocal
-set port=8181
-netsh http add urlacl url=http://*:%port%/ user=everyone >nul
-netsh http add urlacl url=http://*:%port%/ user=alle >nul
-ECHO Din firewall tillader nu trafik gennem port %port%
-endlocal
-pause >nul
